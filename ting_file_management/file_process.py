dec30aeff2166945069274c9ff452caee24b5ff1
Lucas <lucas_emanuel_sc@hotmail.com>
2023-05-08 16:19:12 -0300

    feat: req3 process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,21 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    items = txt_importer(path_file)
+    already_exists = []
+    for index in range(len(instance)):
+        if instance.search(index)["nome_do_arquivo"] == path_file:
+            already_exists.append(instance.search(index)["nome_do_arquivo"])
+    if path_file not in already_exists:
+        data = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(items),
+            "linhas_do_arquivo": items,
+            }
+        instance.enqueue(data)
+        sys.stdout.write(str(data))
 
 
 def remove(instance):
