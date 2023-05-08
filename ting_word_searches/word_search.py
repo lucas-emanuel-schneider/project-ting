f817aa603d1fd2893bf503e013ca0a7ff581e588
Lucas <lucas_emanuel_sc@hotmail.com>
2023-05-08 17:43:29 -0300

    feat: 8.2

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,13 @@
-def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
 
 
-def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+def exists_word(word: str, instance: Queue):
+    words_found = []
+
+    return words_found
+
+
+def search_by_word(word: str, instance: Queue):
+    words_found = []
+
+    return words_found
