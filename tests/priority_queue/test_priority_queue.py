c549d2cd8854ea90ed3f9b6ac2ef8720f2b963cb
Lucas <lucas_emanuel_sc@hotmail.com>
2023-05-08 17:29:53 -0300

    feat: req 06

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,34 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
+
+
+item_01 = {
+            "nome_do_arquivo": "dale.txt",
+            "qtd_linhas": 4,
+            "linhas_do_arquivo": ['a', 'b', 'c', 'd', 'e'],
+            }
+item_02_arr = ['a', 'b', 'c', 'd', 'e', 'a', 'b', 'c', 'd', 'e']
+item_02 = {
+            "nome_do_arquivo": "desdale.txt",
+            "qtd_linhas": 10,
+            "linhas_do_arquivo": item_02_arr,
+            }
+item_03 = {
+            "nome_do_arquivo": "ehnois.txt",
+            "qtd_linhas": 10,
+            "linhas_do_arquivo": item_02_arr,
+            }
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    queue_test = PriorityQueue()
+    queue_test.enqueue(item_02)
+    queue_test.enqueue(item_01)
+    assert len(queue_test) == 2
+    assert queue_test.search(0) == item_01
+    assert queue_test.dequeue() == item_01
+    queue_test.enqueue(item_03)
+    queue_test.dequeue() == item_02
+    assert len(queue_test) == 1
+    with pytest.raises(IndexError):
+        queue_test.search(10)
