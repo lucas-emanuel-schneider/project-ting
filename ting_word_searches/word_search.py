2efa9991aaef580284e9a2be0cde8fb2b5846121
Lucas <lucas_emanuel_sc@hotmail.com>
2023-05-09 14:01:08 -0300

    fix: req 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -10,7 +10,7 @@ def create_occurrences(word, index, line, search_type=False):
     return None
 
 
-def exists_word(word: str, instance: Queue):
+def get_search_result(word, instance, bool=False):
     result = []
     for item in range(len(instance)):
         search_result = instance.search(item)
@@ -20,27 +20,17 @@ def exists_word(word: str, instance: Queue):
             "ocorrencias": []
         }
         for index, line in enumerate(search_result['linhas_do_arquivo']):
-            item = create_occurrences(word, index, line)
+            item = create_occurrences(word, index, line, bool)
             if item:
                 obj_response['ocorrencias'].append(item)
         if len(obj_response['ocorrencias']) > 0:
             result.append(obj_response)
         return result
+
+
+def exists_word(word: str, instance: Queue):
+    return get_search_result(word, instance)
 
 
 def search_by_word(word: str, instance: Queue):
-    result = []
-    for item in range(len(instance)):
-        search_result = instance.search(item)
-        obj_response = {
-            "palavra": word,
-            "arquivo": search_result['nome_do_arquivo'],
-            "ocorrencias": []
-        }
-        for index, line in enumerate(search_result['linhas_do_arquivo']):
-            item = create_occurrences(word, index, line, True)
-            if item:
-                obj_response['ocorrencias'].append(item)
-        if len(obj_response['ocorrencias']) > 0:
-            result.append(obj_response)
-        return result
+    return get_search_result(word, instance, True)
