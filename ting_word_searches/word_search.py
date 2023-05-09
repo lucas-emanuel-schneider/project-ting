720fc478ccaab88a2b18fd9b0aeaba1ec75ffabb
Lucas <lucas_emanuel_sc@hotmail.com>
2023-05-09 12:04:38 -0300

    fix: req 07

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,15 @@
 from ting_file_management.queue import Queue
 
 
+def create_occurrences(word, index, line, search_type=False):
+    if word.lower() in line.lower():
+        if search_type:
+            return {'linha': index + 1, "conteudo": line}
+        else:
+            return {'linha': index + 1}
+    return None
+
+
 def exists_word(word: str, instance: Queue):
     result = []
     for item in range(len(instance)):
@@ -11,8 +20,9 @@ def exists_word(word: str, instance: Queue):
             "ocorrencias": []
         }
         for index, line in enumerate(search_result['linhas_do_arquivo']):
-            if word.lower() in line.lower():
-                obj_response['ocorrencias'].append({'linha': index + 1})
+            item = create_occurrences(word, index, line)
+            if item:
+                obj_response['ocorrencias'].append(item)
         if len(obj_response['ocorrencias']) > 0:
             result.append(obj_response)
         return result
@@ -28,10 +38,9 @@ def search_by_word(word: str, instance: Queue):
             "ocorrencias": []
         }
         for index, line in enumerate(search_result['linhas_do_arquivo']):
-            if word.lower() in line.lower():
-                obj_response['ocorrencias'].append(
-                    {'linha': index + 1, "conteudo": line}
-                    )
+            item = create_occurrences(word, index, line, True)
+            if item:
+                obj_response['ocorrencias'].append(item)
         if len(obj_response['ocorrencias']) > 0:
             result.append(obj_response)
         return result
